e5a10d292d05579ade17d43aad2d1d3a29596696
Derek Truong <derektruongdev@gmail.com>
2022-09-01 16:35:54 -0500

    arrays challenges

---
+++ nested_evens.js
@@ -0,0 +1,12 @@
+let myArray = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+];
+let newArr = myArray.flat();
+
+newArr.forEach(num => {
+  if(num % 2 === 0) {
+    console.log(num)
+  }
+})
